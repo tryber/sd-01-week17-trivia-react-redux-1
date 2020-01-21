c567a093ee35227d4e4d7d3d30d5a4903c397e1c
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-21 18:41:49 -0300

    primeiro push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import logo from './trivia.png';
 // import './App.css';
+
 import Home from './components/Home';
 
 export default function App() {
