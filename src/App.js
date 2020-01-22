1b31ce3bc910389ef74d4882132c8bd4833882f2
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-22 16:15:37 -0300

    Colocando o componente Ranking

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Configurations from './components/Configurations';
+import Ranking from './components/Ranking';
 import Game from './components/Game';
 
 export default function App() {
