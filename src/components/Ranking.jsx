ad11e1c1731b775f211388639af3c1300d01b0fe
antoniopaolillo <antonio_francisco_p@live.com>
2020-01-27 11:13:10 -0300

    codeClimate

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import '../css/Ranking.css';
 
 class Ranking extends Component {
-  generateRankingTable() {
+  static generateRankingTable() {
     const ranking = JSON.parse(localStorage.ranking);
-    return ranking.map((position, index) => {
-      return (
-        <div key={position.name} data-testid={`${position.name}-${index}`}>
-          <img src={position.gravatarImg} alt={position.name} />
-          <p>{`${position.name} - ${position.score} pontos`}</p>
-        </div>
-      );
-    });
+    return ranking.map((position, index) => (
+      <div key={position.name} data-testid={`${position.name}-${index}`}>
+        <img src={position.gravatarImg} alt={position.name} />
+        <p>{`${position.name} - ${position.score} pontos`}</p>
+      </div>
+    ));
   }
 
   render() {
     return (
       <div>
         <h1>Ranking</h1>
-        {this.generateRankingTable()}
+        {Ranking.generateRankingTable()}
       </div>
     );
   }
